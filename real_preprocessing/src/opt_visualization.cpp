803b6efe14b26f36fa76eb29f4f48616f973fd3e
DAA2310 <david@virtanatech.com>
2019-08-29 16:57:04 -0400

    visualizer refactor #1

--- real_preprocessing/src/opt_visualization.cpp
+++ real_preprocessing/src/opt_visualization.cpp
@@ -10,20 +10,22 @@
 #include "geometry_msgs/Pose.h"
 #include "geometry_msgs/PoseArray.h"
 
-#define TARGET -1
-
-//Returns the absolute directory path for detections/target yaml files
-std::string getFilepath(int filenum=-1)
+//Returns the absolute directory path for package detections folder
+std::string getBasepath()
 {
-  std::string basepath = ros::package::getPath("real_preprocessing") +"/detections";
-  if (filenum == TARGET)
-  {
-	return basepath + "/targets.yaml";
-  }
-  else
-  {
-    return basepath + "/detections_" + std::to_string(filenum) + ".yaml";
-  }
+return (ros::package::getPath("real_preprocessing") +"/detections");
+}
+
+//Returns the absolute directory path for detections files
+std::string getDetfilePath(int filenum)
+{
+return (getBasepath() + "/detections_" + std::to_string(filenum) + ".yaml");
+}
+
+//Returns the absolute directory path for targets yaml file
+std::string getTargetpath()
+{
+return (getBasepath() + "/targets.yaml");
 }
 
 //converts and returns a quaternion given a rodrigues angle
@@ -40,10 +42,11 @@ Eigen::Quaterniond rodrigToQuat(double rod_vec[3])
 	return quat;
 }
 
-//populates a pose message given a tag index in the targets.yaml file
-void LoadmarkerPose(int tag_index, geometry_msgs::Pose& pose_msg)
+//generates a pose message given a tag index in the targets.yaml file
+geometry_msgs::Pose LoadmarkerPose(int tag_index)
 {
-	YAML::Node YAML_handle= YAML::LoadFile(getFilepath());
+	geometry_msgs::Pose pose_msg;
+	YAML::Node YAML_handle= YAML::LoadFile(getTargetpath());
 	
 	pose_msg.position.x = YAML_handle["targets"][tag_index]["world_T_target"]["translation"][0].as<double>();
 	pose_msg.position.y = YAML_handle["targets"][tag_index]["world_T_target"]["translation"][1].as<double>();
@@ -58,12 +61,15 @@ void LoadmarkerPose(int tag_index, geometry_msgs::Pose& pose_msg)
 	pose_msg.orientation.y = quat.y();
 	pose_msg.orientation.z = quat.z();
 	pose_msg.orientation.w = quat.w();
+
+	return pose_msg;
 }
 
 //populates a visualization marker message given a pose and tag index in the targets.yaml file 
-void editMarker(int tag_index, visualization_msgs::Marker& marker, geometry_msgs::Pose& pose_msg)
+void loadTagmarker(int tag_index, visualization_msgs::Marker& marker, geometry_msgs::Pose& pose_msg)
 {
-	YAML::Node YAML_handle= YAML::LoadFile(getFilepath());
+	YAML::Node YAML_handle= YAML::LoadFile(getTargetpath());
+	pose_msg = LoadmarkerPose(tag_index);
 
 	marker.header.frame_id = "world"; 
 	marker.header.stamp = ros::Time();
@@ -80,11 +86,13 @@ void editMarker(int tag_index, visualization_msgs::Marker& marker, geometry_msgs
 	marker.color.g = 1;
 	marker.color.b = 1;
 	marker.lifetime = ros::Duration();
+
 }
 
 //updates the trajectory marker with all camera poses
-void trajMarker(geometry_msgs::PoseArray& campose_array, visualization_msgs::Marker& traj_marker)
+visualization_msgs::Marker trajMarker(geometry_msgs::PoseArray campose_array)
 {
+	visualization_msgs::Marker traj_marker;
 	traj_marker.header.frame_id = "world"; 
 	traj_marker.header.stamp = ros::Time();
 	traj_marker.ns = "camera_trajectory";
@@ -92,7 +100,7 @@ void trajMarker(geometry_msgs::PoseArray& campose_array, visualization_msgs::Mar
 	traj_marker.type = visualization_msgs::Marker::LINE_STRIP;
 	traj_marker.action = visualization_msgs::Marker::ADD;
 	traj_marker.scale.x = 0.002;
-	traj_marker.color.a = 1.0; // Don't forget to set the alpha!
+	traj_marker.color.a = 1.0;
 	traj_marker.color.r = 1;
 	traj_marker.color.g = 1;
 	traj_marker.color.b = 0;
@@ -104,52 +112,51 @@ void trajMarker(geometry_msgs::PoseArray& campose_array, visualization_msgs::Mar
 	{
 		traj_marker.points.push_back(campose_array.poses[i].position);
 	}
+	return traj_marker;
+}
+
+geometry_msgs::Pose getCampose(int filenum)
+{
+	YAML::Node YAML_handle = YAML::LoadFile(getDetfilePath(filenum));
+
+	geometry_msgs::Pose cam_pose;
+	cam_pose.position.x = YAML_handle["world_T_camera"]["translation"][0].as<double>();
+	cam_pose.position.y = YAML_handle["world_T_camera"]["translation"][1].as<double>();
+	cam_pose.position.z = YAML_handle["world_T_camera"]["translation"][2].as<double>();
+
+	double rod_vec[3] = {YAML_handle["world_T_camera"]["rotation"][0].as<double>(),
+							YAML_handle["world_T_camera"]["rotation"][1].as<double>(),
+							YAML_handle["world_T_camera"]["rotation"][2].as<double>()};
+
+	Eigen::Quaterniond quat = rodrigToQuat(rod_vec);
+	cam_pose.orientation.x = quat.x();
+	cam_pose.orientation.y = quat.y();
+	cam_pose.orientation.z = quat.z();
+	cam_pose.orientation.w = quat.w();
+
+	return cam_pose;
 }
 
 //populates camera marker with world_T_cam from given file number
-void camUpdate(int& filenum, visualization_msgs::Marker& cam_marker, geometry_msgs::PoseArray& campose_array)
+void camUpdate(int filenum, visualization_msgs::Marker& cam_marker, geometry_msgs::Pose& cam_pose)
 {
-    std::ifstream fin;
-    fin.open(getFilepath(filenum));
-    if (fin.is_open())
-    {
-    	fin.close();
-		YAML::Node YAML_handle = YAML::LoadFile(getFilepath(filenum));
+	cam_pose = getCampose(filenum);
 
-		geometry_msgs::Pose cam_pose;
-		cam_pose.position.x = YAML_handle["world_T_camera"]["translation"][0].as<double>();
-		cam_pose.position.y = YAML_handle["world_T_camera"]["translation"][1].as<double>();
-		cam_pose.position.z = YAML_handle["world_T_camera"]["translation"][2].as<double>();
-
-		double rod_vec[3] = {YAML_handle["world_T_camera"]["rotation"][0].as<double>(),
-							 YAML_handle["world_T_camera"]["rotation"][1].as<double>(),
-							 YAML_handle["world_T_camera"]["rotation"][2].as<double>()};
-
-		Eigen::Quaterniond quat = rodrigToQuat(rod_vec);
-		cam_pose.orientation.x = quat.x();
-		cam_pose.orientation.y = quat.y();
-		cam_pose.orientation.z = quat.z();
-		cam_pose.orientation.w = quat.w();
-		campose_array.poses.push_back(cam_pose);
-	
-		cam_marker.header.frame_id = "world"; 
-		cam_marker.header.stamp = ros::Time();
-		cam_marker.ns = "camera";
-		cam_marker.id = -1;
-		cam_marker.type = visualization_msgs::Marker::CUBE;
-		cam_marker.action = visualization_msgs::Marker::ADD;
-		cam_marker.pose = cam_pose;
-		cam_marker.scale.x = 0.08;
-		cam_marker.scale.y = 0.08;
-		cam_marker.scale.z = 0.04;
-		cam_marker.color.a = 1.0; 
-		cam_marker.color.r = 1.00;
-		cam_marker.color.g = 0.41;
-		cam_marker.color.b = 0.71;
-		cam_marker.lifetime = ros::Duration();
-		
-		filenum++;
-	}
+	cam_marker.header.frame_id = "world"; 
+	cam_marker.header.stamp = ros::Time();
+	cam_marker.ns = "camera";
+	cam_marker.id = -1;
+	cam_marker.type = visualization_msgs::Marker::CUBE;
+	cam_marker.action = visualization_msgs::Marker::ADD;
+	cam_marker.pose = cam_pose;
+	cam_marker.scale.x = 0.08;
+	cam_marker.scale.y = 0.08;
+	cam_marker.scale.z = 0.04;
+	cam_marker.color.a = 1.0; 
+	cam_marker.color.r = 1.00;
+	cam_marker.color.g = 0.41;
+	cam_marker.color.b = 0.71;
+	cam_marker.lifetime = ros::Duration();
 }
 
 int main(int argc, char **argv)
@@ -161,12 +168,11 @@ int main(int argc, char **argv)
 	ros::Publisher cam_pub = nh.advertise<visualization_msgs::Marker>("cam_vis_marker", 1000);
 	ros::Publisher cam_ps_pub = nh.advertise<geometry_msgs::PoseArray>("world_T_cam_ps", 1000);
 	ros::Publisher cam_traj_pub = nh.advertise<visualization_msgs::Marker>("cam_traj", 1000);
-	ros::Rate loop_rate(10);
+	ros::Rate loop_rate(5);
 	
 	int filenum(0);
 	visualization_msgs::MarkerArray tag_array;
 	visualization_msgs::Marker cam_marker;
-	visualization_msgs::Marker cam_traj;
 	visualization_msgs::Marker traj_marker;
 	geometry_msgs::PoseArray tagpose_array;
 	geometry_msgs::PoseArray campose_array;
@@ -174,34 +180,42 @@ int main(int argc, char **argv)
 	tagpose_array.header.frame_id = "world";
 	campose_array.header.frame_id = "world";
 
+	// Publish the world to tags from the targets.yaml
+	YAML::Node YAML_handle= YAML::LoadFile(getTargetpath());
+	for(int tag_index = 0; tag_index != YAML_handle["targets"].size(); tag_index++) 
+	{
+		geometry_msgs::Pose tag_pose;
+		visualization_msgs::Marker tag_marker;
+		loadTagmarker(tag_index, tag_marker, tag_pose);
+
+		tag_array.markers.push_back(tag_marker);
+		tagpose_array.poses.push_back(tag_pose);
+	}
+
+	// Continuously publish the world to Cam for each detection file
 	while (ros::ok())
-	{		
-		YAML::Node YAML_handle= YAML::LoadFile(getFilepath());
-	 	for(int tag_index = 0; tag_index != YAML_handle["targets"].size(); tag_index++) 
-	 	{
-			visualization_msgs::Marker tag_marker;
-			geometry_msgs::Pose tag_marker_pose;
-
-			LoadmarkerPose (tag_index, tag_marker_pose);
-			editMarker(tag_index, tag_marker, tag_marker_pose);
-
-			tag_array.markers.push_back(tag_marker);
-			tagpose_array.poses.push_back(tag_marker_pose);
-	 	}
-
-		camUpdate(filenum, cam_marker, campose_array);
-		trajMarker(campose_array, traj_marker);
-
+	{
 		tag_pub.publish(tag_array); 
 		tag_ps_pub.publish(tagpose_array);
-		cam_pub.publish(cam_marker);
-		cam_ps_pub.publish(campose_array);
-		cam_traj_pub.publish(traj_marker);
-		
-		tagpose_array.poses.clear();
-		tag_array.markers.clear();
 
-		loop_rate.sleep();
+		std::ifstream fin;
+		fin.open(getDetfilePath(filenum));
+		if (fin.is_open())
+		{
+			geometry_msgs::Pose cam_pose;
+			visualization_msgs::Marker cam_marker;
+			camUpdate(filenum, cam_marker, cam_pose);
+			campose_array.poses.push_back(cam_pose);
+			visualization_msgs::Marker traj_marker = trajMarker(campose_array); 
+
+			cam_pub.publish(cam_marker);
+			cam_ps_pub.publish(campose_array);
+			cam_traj_pub.publish(traj_marker);
+			
+			filenum++;
+			
+		}
+		loop_rate.sleep(); 
 	}
  	return 0;
 }
